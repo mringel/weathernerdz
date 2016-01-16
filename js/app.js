d2d7c87809a6f07b0068bd4744cc52a8d05ae69a
Matthew Ringel <mattew.r.ringel@gmail.com>
2016-01-16 12:23:06 -0800

    trying doubleclick

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
 angular.module('myApp', ['nemLogging', 'ui-leaflet'])
-  .controller('MyController', function($scope) {
+  .controller('MyController', ['$scope', 'leafletData',
+    function($scope,leafletData) {
 
+      $scope.position = {};
+      leafletData.getMap().then(function(map) {
+        map.on('dblclick', function(e) {
+          $scope.position.lat = e.latlng.lat;
+          $scope.position.long = e.latlng.lng;
+        });
+      });
 
+      angular.extend($scope, {
+        seattle: {
+          lat: 47.6,
+          lng: -122.33,
+          zoom: 9
+        },
 
-    angular.extend($scope, {
-      seattle: {
-        lat: 47.6,
-        lng: -122.33,
-        zoom: 9
-      },
-
-      defaults: {
-        scrollWheelZoom: false,
-        doubleClickZoom: false,
-        tap: false,
-        tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        maxZoom: 14
-      }
-    });
-  });
+        defaults: {
+          scrollWheelZoom: false,
+          doubleClickZoom: true,
+          tap: false,
+          tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          maxZoom: 14
+        }
+      });
+    }]);
