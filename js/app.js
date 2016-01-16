55ca44f2b865658ebff2a24bfb532a2a812b7287
Matthew Ringel <mattew.r.ringel@gmail.com>
2016-01-16 12:04:50 -0800

    added more base structure

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 angular.module('myApp', ['nemLogging', 'ui-leaflet'])
   .controller('MyController', function($scope) {
 
+
+
     angular.extend($scope, {
       seattle: {
         lat: 47.6,
         lng: -122.33,
-        zoom: 12
+        zoom: 9
       },
 
       defaults: {
         scrollWheelZoom: false,
+        doubleClickZoom: false,
         tap: false,
         tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         maxZoom: 14
