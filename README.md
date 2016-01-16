dc96a896a8d055b4fb7db7a63aa054eeeecdb997
Matthew Ringel <mattew.r.ringel@gmail.com>
2016-01-16 15:58:31 -0800

    basic plotly plot working, not tied to weather API yet

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # weathernerdz
 
 a sample weather app that uses the Dark Sky Forecast API, leaflet, angular, and plotly
+
+
+found bug in leaflet 1.0.0 beta 2 when using chrome on a touch screen
+
+Plotly doesn't work with webpack.  
--- app/js/weather/controllers/data_controller.js
+++ app/js/weather/controllers/data_controller.js
@@ -1,8 +1,28 @@
 module.exports = function(app) {
   app.controller('MyController', ['$scope', 'leafletData',
     function($scope,leafletData) {
-
       var darkSkyKey = 'ddd77ff3c434ad2c1efc34ede4e8e016';
+
+      var sampleData = require('../sampledata');
+      $scope.forecast = sampleData;
+
+      var time = sampleData.hourly.data.map(function(obj) {
+        return obj.time;
+      });
+
+      var apparentTemperature = sampleData.hourly.data.map(function(obj) {
+        return obj.apparentTemperature;
+      });
+
+      var plotData = [
+        {
+          x: time,
+          y: apparentTemperature,
+          type: 'scatter'
+        }
+      ];
+
+      Plotly.newPlot('plot', plotData);
 
       $scope.position = {};
       leafletData.getMap().then(function(map) {
