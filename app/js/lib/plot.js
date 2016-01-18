287f3bca3e41ea984afc9e50ea89ea46fb4709b0
Matthew Ringel <mattew.r.ringel@gmail.com>
2016-01-17 16:32:27 -0800

    refactored to put plot functionality in separate module

--- app/js/lib/plot.js
+++ app/js/lib/plot.js
@@ -2,21 +2,76 @@ var moment = require('moment');
 
 module.exports = function() {
 
-  this.mapTime = function(forecastObj) {
+  var mapTime = function(forecastObj) {
     return forecastObj.hourly.data.map(function(obj) {
       return moment(obj.time*1000).format('Do h:mm a');
     });
   };
 
-  this.mapApparentTemp = function(forecastObj) {
+  var mapApparentTemp = function(forecastObj) {
     return forecastObj.hourly.data.map(function(obj) {
       return obj.apparentTemperature;
     });
   };
 
-  this.mapField = function(forecastObj, field) {
+  var mapField = function(forecastObj, field) {
     return forecastObj.hourly.data.map(function(obj) {
         return obj[field];
     });
   };
-};
+
+  this.plotTemp = function(dataObj) {
+    var time = mapTime(dataObj);
+    var apparentTemperature = mapApparentTemp(dataObj);
+
+    var tempPlotData = [
+      {
+        x: time,
+        y: apparentTemperature,
+        name: 'Apparent Temperature',
+        type: 'scatter'
+      }
+    ];
+
+    var tempLayout = {
+      title: 'Apparent Temperature Hourly Forecast',
+      xaxis: {
+        title: 'Date'
+      },
+      yaxis: {
+        title: 'Apparent Temperature'
+      }
+    };
+
+    Plotly.newPlot('temp-plot', tempPlotData, tempLayout);
+
+  }; //end plotTemp
+
+  this.plotHumidity = function(dataObj) {
+    var time = mapTime(dataObj);
+    var humidity = mapField(dataObj, 'humidity');
+
+    var humidityPlotData = [
+      {
+        x: time,
+        y: humidity,
+        name: 'Humidity',
+        type: 'scatter'
+      }
+    ];
+
+    var humidityLayout = {
+      title: 'Humidity Hourly Forecast',
+      xaxis: {
+        title: 'Date'
+      },
+      yaxis: {
+        title: 'Humidity'
+      }
+    };
+
+    Plotly.newPlot('humidity-plot', humidityPlotData, humidityLayout);
+
+  }; //end plotHumidity
+
+}; // end constructor
--- app/js/weather/controllers/data_controller.js
+++ app/js/weather/controllers/data_controller.js
@@ -1,6 +1,8 @@
 module.exports = function(app) {
   app.controller('MyController', ['$scope', 'leafletData',
     function($scope,leafletData) {
+
+      $scope.inputDate = $scope.inputDate || new Date();
       var darkSkyKey = 'ddd77ff3c434ad2c1efc34ede4e8e016';
 
       var Plot = require('../../lib/plot');
@@ -9,51 +11,10 @@ module.exports = function(app) {
       var sampleData = require('../sampledata');
       // $scope.forecast = sampleData;
 
-      var time = plot.mapTime(sampleData);
-      var apparentTemperature = plot.mapApparentTemp(sampleData);
-      var humidity = plot.mapField(sampleData, 'humidity');
+      plot.plotTemp(sampleData);
+      plot.plotHumidity(sampleData);
 
-      var tempPlotData = [
-        {
-          x: time,
-          y: apparentTemperature,
-          name: 'Apparent Temperature',
-          type: 'scatter'
-        }
-      ];
 
-      var tempLayout = {
-        title: 'Apparent Temperature Hourly Forecast',
-        xaxis: {
-          title: 'Date'
-        },
-        yaxis: {
-          title: 'Apparent Temperature'
-        }
-      };
-
-      Plotly.newPlot('temp-plot', tempPlotData, tempLayout);
-
-      var humidityPlotData = [
-        {
-          x: time,
-          y: humidity,
-          name: 'Humidity',
-          type: 'scatter'
-        }
-      ];
-
-      var humidityLayout = {
-        title: 'Humidity Hourly Forecast',
-        xaxis: {
-          title: 'Date'
-        },
-        yaxis: {
-          title: 'Humidity'
-        }
-      };
-
-      Plotly.newPlot('humidity-plot', humidityPlotData, humidityLayout);
 
       $scope.position = {};
       leafletData.getMap().then(function(map) {
